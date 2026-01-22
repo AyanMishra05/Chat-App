12866ab89fe54674b4457ec2324b23d9e87c01d6
Ayan Mishra <ayanmishra2021@gmail.com>
2026-01-22 18:08:08 +0530

    Added the profile page UI

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProfileUpdate.css";
+import assets from "../../assets/assets/assets.js";
+
 const ProfileUpdate = () => {
-  return <div>ON the profile page</div>;
+  const [image, setImage] = useState(false);
+
+  return (
+    <div className="profile">
+      <div className="profile-container">
+        <form>
+          <h3>Profile Details</h3>
+          <label htmlFor="avatar">
+            <input
+              onChange={(e) => setImage(e.target.files[0])}
+              type="file"
+              name=""
+              id="avatar"
+              accept=".png, .jpg, .jpeg"
+              hidden
+            />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.avatar_icon}
+              alt=""
+            />
+            Upload profile image
+          </label>
+          <input type="text" placeholder="Your Name" required name="" id="" />
+          <textarea placeholder="Write profile bio" required></textarea>
+          <button type="submit">Save</button>
+        </form>
+        <img
+          className="profile-pic"
+          src={image ? URL.createObjectURL(image) : assets.logo_icon}
+          alt=""
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ProfileUpdate;
